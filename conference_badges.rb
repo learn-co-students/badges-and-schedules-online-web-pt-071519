411d652f970b11cf0b5085cf9e925be01f051a29
Paul Beckwith <303dog@gmail.com>
2019-07-23 19:54:06 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+attendees.map do |name|
+  badge_maker(name)
+  counter = 1
+def assign_rooms = [1,2,3,4,5,6,7]
+ return "Hello,#{name}!You'll be assigned to room #{assign_rooms}!"
+ counter += 1 
+end
+end
+
+
+
+
+#counter = 1 
+#assign_rooms = [1,2,3,4,5,6,7]
+#puts "Hello, #{name}! You'll be assigned to room #{room}!"
+#counter += 1
