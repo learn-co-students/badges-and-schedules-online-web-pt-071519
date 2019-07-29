3077718361f7b807b795d166c24417301a7a6057
David Jermaine Whitehead <david_whitehead_234@yahoo.com>
2019-07-29 17:58:54 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(speakers)
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "linus", "Matz"]
+  
+  speakers.do |speaker|
+    puts "Hello, my name is #{speaker}."
+    
+  end
+  
+end
